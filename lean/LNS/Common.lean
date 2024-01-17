6584e36ff1a7ff97da405a915ef36ae394eb8cb4
Alexey Solovyev <solovyev.alexey@gmail.com>
2024-01-16 17:14:37 -0700

    Add common lemmas about Int.ceil

--- lean/LNS/Common.lean
+++ lean/LNS/Common.lean
@@ -64,3 +64,45 @@ lemma monotone_restrict [Preorder α] [Preorder β] {f : α → β} {s : Set α}
 
 lemma antitone_restrict [Preorder α] [Preorder β] {f : α → β} {s : Set α} :
     Antitone (s.restrict f) ↔ AntitoneOn f s := by simp [Set.restrict, Antitone, AntitoneOn]
+
+/- Int.ceil lemmas -/
+
+namespace Int
+
+variable {F α β : Type*}
+variable [LinearOrderedRing α] [FloorRing α] {z : ℤ} {a : α}
+
+lemma ceil_nonpos (ha : a ≤ 0) : ⌈a⌉ ≤ 0 := by
+  rw [ceil_le, cast_zero]; exact ha
+
+lemma floor_eq_self : ↑⌊a⌋ = a ↔ fract a = 0 := by
+  simp only [fract, sub_eq_zero, eq_comm]
+
+lemma ceil_eq_floor_iff_frac_eq_zero : ⌈a⌉ = ⌊a⌋ ↔ fract a = 0 := by
+  constructor <;> intro h
+  · exact le_antisymm (sub_nonpos_of_le $ h ▸ le_ceil _) (fract_nonneg a)
+  · rw [ceil_eq_iff, floor_eq_self.mpr h]
+    exact ⟨sub_one_lt _, le_refl _⟩
+
+lemma ceil_eq_self : ↑⌈a⌉ = a ↔ fract a = 0 := by
+  constructor <;> intro h
+  · rw [← ceil_eq_floor_iff_frac_eq_zero]
+    apply le_antisymm _ (floor_le_ceil a)
+    rw [← @Int.cast_le α, h, ←h, floor_intCast]
+  · rw [ceil_eq_floor_iff_frac_eq_zero.mpr h, floor_eq_self.mpr h]
+
+section LinearOrderedField
+
+variable {k : Type*} [LinearOrderedField k] [FloorRing k] {b : k}
+
+lemma ceil_div_mul_sub_lt {a b : k} (hb : 0 < b) : ⌈a / b⌉ * b - a < b := by
+  rw [sub_lt_iff_lt_add, ←lt_div_iff hb, same_add_div (ne_of_gt hb), add_comm]
+  exact ceil_lt_add_one _
+
+lemma ceil_div_mul_sub_nonneg {a b : k} (hb : 0 < b) : 0 ≤ ⌈a / b⌉ * b - a := by
+  rw [sub_nonneg, ← div_le_iff hb]
+  exact le_ceil _
+
+end LinearOrderedField
+
+end Int
